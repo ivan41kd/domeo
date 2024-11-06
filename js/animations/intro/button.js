5db6011a9c35ae6547e012c1c52ec7e0df4dfc19
ivan41kd <grkorolevskiy@gmail.com>
2024-11-06 10:18:31 +0300

    test

--- js/animations/intro/button.js
+++ js/animations/intro/button.js
@@ -2,6 +2,8 @@ const introButton = document.querySelector('.intro__button');
 const enableAnim = () => {
  const buttonWrapper = document.querySelector('.intro__button-wrapper');
  setTimeout(() => {
+  buttonWrapper.style.transition = '1.2s';
+  introButton.style.transition = '1.2s';
   buttonWrapper.classList.add('middle');
   buttonWrapper.classList.remove('middle');
   buttonWrapper.classList.add('end');
