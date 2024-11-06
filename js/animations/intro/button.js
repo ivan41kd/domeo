f5cb746783ad9335ec51e0dc808de459a41d2c5a
ivan41kd <grkorolevskiy@gmail.com>
2024-11-06 10:22:09 +0300

    fix: add timeout for button, video in intro

--- js/animations/intro/button.js
+++ js/animations/intro/button.js
@@ -1,11 +1,13 @@
 const introButton = document.querySelector('.intro__button');
 const enableAnim = () => {
  const buttonWrapper = document.querySelector('.intro__button-wrapper');
- buttonWrapper.style.transition = '1.2s';
- introButton.style.transition = '1.2s';
- buttonWrapper.classList.add('middle');
- buttonWrapper.classList.remove('middle');
- buttonWrapper.classList.add('end');
+ setTimeout(() => {
+  buttonWrapper.style.transition = '1.2s';
+  introButton.style.transition = '1.2s';
+  buttonWrapper.classList.add('middle');
+  buttonWrapper.classList.remove('middle');
+  buttonWrapper.classList.add('end');
+ }, 600);
 };
 
 document.addEventListener('DOMContentLoaded', enableAnim);
